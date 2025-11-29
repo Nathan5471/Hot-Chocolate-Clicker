5babb0d1b03f7b13a8e743eaae348088d2d7d564
Nathan Cloutier <nathanc4571@gmail.com>
2025-11-28 20:05:24 -0500

    Make basic framework

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,31 @@
 function App() {
-  return <div className="text-4xl text-red-500">Hi!</div>;
+  return (
+    <div className="w-screen h-screen flex flex-row">
+      <div className="w-2/3 flex flex-col items-center justify-center bg-primary-a0">
+        <div className="w-1/2 aspect-square flex items-center justify-center bg-primary-a1 rounded-lg p-4">
+          <img
+            src="/hotChocolate.png"
+            alt="Hot Chocolate"
+            className="w-11/12 h-11/12 hover:scale-110 transition-transform"
+          />
+        </div>
+      </div>
+      <div className="w-1/3 flex-col items-center bg-primary-a1 p-6">
+        <h1 className="text-3xl font-bold text-center mb-2">
+          100000 Hot Chocolates
+        </h1>
+        <div className="w-full h-full overflow-y-auto flex flex-col">
+          <button className="w-full h-20 flex flex-col bg-primary-a2 hover:bg-primary-a3 transition-colors rounded-lg p-2">
+            <div className="flex flex-row w-full">
+              <h2 className="text-lg text-left font-bold">Upgrade #1 (3)</h2>
+              <p className="ml-auto">5000 hcs</p>
+            </div>
+            <p>This does some cool stuff</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
